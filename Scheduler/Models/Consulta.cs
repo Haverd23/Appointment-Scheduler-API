1f6336511660b6b24b092ce2f7f59c97c8b11c3c
João <GabrielAlvees616@gmail.com>
2025-03-02 13:05:34 -0300

    Criado model Consulta e tabela Consultas

---
+++ Scheduler/Models/Consulta.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scheduler.Models
+{
+    public class Consulta
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PacienteId { get; set; }
+        public int MedicoId { get; set; }
+        public DateTime DataHora { get; set; }
+        public string Status { get; set; }
+        public User Medico { get; set; }
+        public User Paciente { get; set; }
+    }
+}
